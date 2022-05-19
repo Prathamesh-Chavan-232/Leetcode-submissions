644538a22238223c3ebf120ebe212a18f7ae319d
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-05-19 16:46:30 +0530

    Time: 86 ms (34.71%), Space: 27.3 MB (18.54%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -1,31 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortening syntax
+#define ll long long
+#define fo(i, n) for (ll i = 0; i < n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
+#define pb push_back
+#define mp make_pair
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vector<int>, greater<int>>
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define foreach(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define formap(m) for (auto [key, value] : m)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+// Typdefs for containers
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+// Varidiac Variable debugger
+#ifndef ONLINE_JUDGE
+#define debug(...) logger(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+template <typename... Args>
+void logger(string varname, Args &&...values) // logger for varadiac debugging print statements
+{
+
+    cerr << varname << " =";
+    string delim = " ";
+    (..., (cerr << delim << values, delim = ", "));
+    cerr << "\n";
+}
+
+// STL vector / set (of any type) debugger
+#ifndef ONLINE_JUDGE
+#define debcon(x)        \
+    cerr << #x << " = "; \
+    _print(x);           \
+    cerr << "\n";
+#else
+#define debcon(x)
+#endif
+template <typename T>
+void _print(T const &c)
+{
+    cerr << "{ ";
+    foreach (it, c)
+    {
+        cerr << *it << ", ";
+    }
+    cerr << "}";
+}
+
+// Function definitions
+void Add_edge(int v1, int v2);
+void dfs(int vertex);
+
+// constants
+const int mod = 1'000'000'007;
+const int N = 1e5 + 10, M = N;
+const double PI = 3.1415926535897932384626;
+
+vpii graph[N]; // For Adjacency List
+bool vis[N];
+
+/*
+    Link -
+    Problem -
+    Difficulty -
+    contest -
+    Status -
+    Date -
+*/
+/*  Approach -
+
+*/
+
 class Solution
 {
-public:
-    int moves[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    vector<vector<int>> dp; // dp[i][j] will store maximum path length starting from matrix[i][j]
     int maxPath, n, m;
+    vvi dp;
+
+public:
+    int dfs(vvi &matrix, int r, int c)
+    {
+        if (dp[r][c])
+        {
+            return dp[r][c];
+        }
+        dp[r][c] = 1;
+        vpii steps = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for (auto [i, j] : steps)
+        {
+            int new_r = r + i;
+            int new_c = c + j;
+            if (new_r < 0 || new_c < 0 || new_r >= matrix.size() || new_c >= matrix[r].size() || matrix[new_r][new_c] <= matrix[r][c])
+            {
+                continue;
+            }
+            dp[r][c] = max(dp[r][c], 1 + dfs(matrix, new_r, new_c));
+        }
+        return dp[r][c];
+    }
     int longestIncreasingPath(vector<vector<int>> &matrix)
     {
         maxPath = 0, n = size(matrix), m = size(matrix[0]);
         dp.resize(n, vector<int>(m));
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
-                maxPath = max(maxPath, solve(matrix, i, j));
+                maxPath = max(maxPath, dfs(matrix, i, j));
         return maxPath;
-    }
-    int solve(vector<vector<int>> &mat, int i, int j)
-    {
-        if (dp[i][j])
-            return dp[i][j]; // return if result is already calculated
-        dp[i][j] = 1;        // minimum path from each cell is always atleast 1
-       
-        for (int k = 0; k < 4; k++)
-        {
-            int new_i = i + moves[k][0], new_j = j + moves[k][1];
-            if (new_i < 0 || new_j < 0 || new_i >= n || new_j >= m || mat[new_i][new_j] <= mat[i][j])
-                continue;
-            dp[i][j] = max(dp[i][j], 1 + solve(mat, new_i, new_j));
-        }
-        return dp[i][j];
     }
 };
