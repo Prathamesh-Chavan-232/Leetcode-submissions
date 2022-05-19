9fe82cd56de9053260af396ee5e5ef593e01519e
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-05-19 16:44:35 +0530

    Time: 108 ms (26.04%), Space: 16.1 MB (55.62%) - LeetHub

--- 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/329-longest-increasing-path-in-a-matrix.cpp
@@ -10,10 +10,10 @@ public:
         dp.resize(n, vector<int>(m));
         for (int i = 0; i < n; i++)
             for (int j = 0; j < m; j++)
-                maxPath = max(maxPath, solve(matrix, i, j, -1));
+                maxPath = max(maxPath, solve(matrix, i, j));
         return maxPath;
     }
-    int solve(vector<vector<int>> &mat, int i, int j, int prev)
+    int solve(vector<vector<int>> &mat, int i, int j)
     {
         if (dp[i][j])
             return dp[i][j]; // return if result is already calculated
@@ -24,7 +24,7 @@ public:
             int new_i = i + moves[k][0], new_j = j + moves[k][1];
             if (new_i < 0 || new_j < 0 || new_i >= n || new_j >= m || mat[new_i][new_j] <= mat[i][j])
                 continue;
-            dp[i][j] = max(dp[i][j], 1 + solve(mat, new_i, new_j, mat[i][j]));
+            dp[i][j] = max(dp[i][j], 1 + solve(mat, new_i, new_j));
         }
         return dp[i][j];
     }
