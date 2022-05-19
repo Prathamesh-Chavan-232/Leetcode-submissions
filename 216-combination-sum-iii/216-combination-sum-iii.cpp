3ce7aba8e2e3aa032b7d801aee9612e6df703040
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-05-19 17:14:49 +0530

    Time: 4 ms (37.27%), Space: 9.2 MB (9.69%) - LeetHub

---
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortening syntax
+#define ll long long
+#define fo(i, n) for (ll i = 0; i < n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
+#define pb push_back
+#define mp make_pair
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vector<int>, greater<int>>
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define foreach(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define formap(m) for (auto [key, value] : m)
+mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+// Typdefs for containers
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+// Varidiac Variable debugger
+#ifndef ONLINE_JUDGE
+#define debug(...) logger(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+template <typename... Args>
+void logger(string varname, Args &&...values) // logger for varadiac debugging print statements
+{
+
+    cerr << varname << " =";
+    string delim = " ";
+    (..., (cerr << delim << values, delim = ", "));
+    cerr << "\n";
+}
+
+// STL vector / set (of any type) debugger
+#ifndef ONLINE_JUDGE
+#define debcon(x)        \
+    cerr << #x << " = "; \
+    _print(x);           \
+    cerr << "\n";
+#else
+#define debcon(x)
+#endif
+template <typename T>
+void _print(T const &c)
+{
+    cerr << "{ ";
+    foreach (it, c)
+    {
+        cerr << *it << ", ";
+    }
+    cerr << "}";
+}
+
+// Function definitions
+void Add_edge(int v1, int v2);
+void dfs(int vertex);
+
+// constants
+const int mod = 1'000'000'007;
+const int N = 1e5 + 10, M = N;
+const double PI = 3.1415926535897932384626;
+
+vpii graph[N]; // For Adjacency List
+bool vis[N];
+
+/*
+    Link -
+    Problem -
+    Difficulty -
+    contest -
+    Status -
+    Date -
+*/
+/*  Approach -
+
+*/
+
+class Solution
+{
+    vvi res;
+
+public:
+    void generate(vi &combination, int k, int n, int sum, int i)
+    {
+        if (k == 0)
+        {
+            if (sum == n)
+            {
+                res.pb(combination);
+            }
+            return;
+        }
+        if (i > 9)
+            return;
+
+        combination.pb(i);
+        sum += i;
+
+        generate(combination, k - 1, n, sum, i + 1);
+        
+        combination.pop_back();
+        sum -= i;
+        
+        generate(combination, k, n, sum, i + 1);
+    }
+    vector<vector<int>> combinationSum3(int k, int n)
+    {
+        vi combination;
+        int sum = 0;
+        generate(combination, k, n, sum, 1);
+        return res;
+    }
+};
