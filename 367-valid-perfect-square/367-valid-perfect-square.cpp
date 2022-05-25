ab990ad5b1787886e7c021f9aad75e8b22d1fe4b
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-05-25 18:50:15 +0530

    Time: 4 ms (21.93%), Space: 5.9 MB (17.65%) - LeetHub

---
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortening syntax
+#define fo(i, n) for (int i = 0; i < n; ++i)
+#define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
+#define ll long long
+#define pb push_back
+#define mp make_pair
+#define pqb priority_queue<int>
+#define pqs priority_queue<int, vector<int>, greater<int>>
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
+
+// Typdefs for containers
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef map<int, int> mii;
+typedef map<ll, ll> ml;
+typedef set<ll> sl;
+typedef unordered_map<int, int> umii;
+typedef unordered_map<ll, ll> uml;
+typedef unordered_set<ll> usl;
+
+// Debugging tools
+#ifndef ONLINE_JUDGE
+#define debug(...) logger(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+
+#ifndef ONLINE_JUDGE
+#define debcon(x)        \
+    cerr << #x << " = "; \
+    _print(x);           \
+    cerr << "\n";
+#else
+#define debcon(x)
+#endif
+
+// Function definitions
+template <typename... Args>
+void logger(string varname, Args &&...values) // logger for varadiac debugging print statements
+{
+
+    cerr << varname << " = ";
+    string delim = "";
+    (..., (cerr << delim << values, delim = ", "));
+    cerr << "\n";
+}
+template <typename T>
+void _print(T const &c) // print any STL container
+{
+    cerr << "{";
+    tr(it, c) cerr << *it << ", ";
+    cerr << "}";
+}
+
+// Varadiac I/O
+template <typename... T>
+void r(T &...args)
+{
+    ((cin >> args), ...);
+}
+template <typename... T>
+void w(T &&...args)
+{
+    ((cout << args << " "), ...);
+    cout << "\n";
+}
+
+// constants
+const int mod = 1'000'000'007;
+const int N = 1e7, M = N;
+const double PI = 3.1415926535897932384626;
+
+/*
+    Link -
+    Problem -
+    Difficulty -
+    contest -
+    Status -
+    Date -
+
+Approach -
+
+*/
+class Solution
+{
+public:
+    bool isPerfectSquare(int n)
+    {
+        bool res = false;
+        int low = 1, high = n;
+        ll target;
+        while (low <= high)
+        {
+            ll mid = low + (high - low) / 2;
+            target = mid * mid;
+            if (target == n)
+            {
+                res = true;
+                break;
+            }
+            else if (target < n)
+            {
+                low = mid + 1;
+            }
+            else
+            {
+                high = mid - 1;
+            }
+        }
+        return res;
+    }
+};
