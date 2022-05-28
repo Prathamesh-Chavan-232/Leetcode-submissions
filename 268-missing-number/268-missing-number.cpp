fed2e653c00d690eba675ca1e6016798f0d26628
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-05-28 09:46:55 +0530

    Time: 26 ms (55.22%), Space: 18.1 MB (20.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,115 +1,15 @@
-// Shortening syntax
-#define ll long long 
-#define fo(i, n) for (int i = 0; i < n; ++i)
-#define Fo(i, k, n) for (int i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
-#define pb push_back
-#define mp make_pair
-#define pqb priority_queue<int>
-#define pqs priority_queue<int, vector<int>, greater<int>>
-#define all(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define tr(it, a) for (auto it = a.begin(); it != a.end(); it++)
-
-// Typdefs for containers
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pii> vpii;
-typedef vector<pl> vpl;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef map<int, int> mii;
-typedef map<ll, ll> ml;
-typedef set<ll> sl;
-typedef unordered_map<int, int> umii;
-typedef unordered_map<ll, ll> uml;
-typedef unordered_set<ll> usl;
-
-// Debugging tools
-#ifndef ONLINE_JUDGE
-#define debug(...) logger(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define debug(...)
-#endif
-
-#ifndef ONLINE_JUDGE
-#define debcon(x)        \
-    cerr << #x << " = "; \
-    _print(x);           \
-    cerr << "\n";
-#else
-#define debcon(x)
-#endif
-
-// Function definitions
-template <typename... Args>
-void logger(string varname, Args &&...values) // logger for varadiac debugging print statements
-{
-
-    cerr << varname << " = ";
-    string delim = "";
-    (..., (cerr << delim << values, delim = ", "));
-    cerr << "\n";
-}
-
-template <typename T>
-void _print(T const &c) // print any STL container
-{
-    cerr << "{";
-    tr(it, c) cerr << *it << ", ";
-    cerr << "}";
-}
-
-// Varadiac I/O
-template <typename... T>
-void r(T &...args)
-{
-    ((cin >> args), ...);
-}
-template <typename... T>
-void w(T &&...args)
-{
-    ((cout << args << " "), ...);
-    cout << "\n";
-}
-
-// constants
-const int mod = 1'000'000'007;
-const int N = 1e7, M = N;
-const double PI = 3.1415926535897932384626;
-
-class Solution
-{
+class Solution {
 public:
-    int missingNumber(vector<int> &arr)
-    {
-        int n = arr.size();
-        int res = n;
-        int i = 0;
-        while(i < n)
-        {
-
-            int correct = arr[i];
-            if ((i != correct) && (arr[i] != n))
-            {
-                swap(arr[correct], arr[i]);
-            }
-            else
-            {
-                i++;
-            }
-            
+    int missingNumber(vector<int>& nums) {
+        int result = nums.size();
+        int i=0;
+        
+        for(int num:nums){
+            result ^= num;
+            result ^= i;
+            i++;
         }
-        Fo(i, 0, n)
-        {
-            if (i != arr[i])
-            {
-                res = i;
-                break;
-            }
-        }
-        return res;
+        
+        return result;
     }
 };
