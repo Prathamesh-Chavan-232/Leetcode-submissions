ad88a0f35378d7023edef5682c909136c35687e6
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-06-10 04:45:44 +0530

    Time: 11 ms (93.16%), Space: 15.6 MB (75.31%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        int N = nums.size();
-        for(int i=0;i<N-1;i++){
-            int lo = i+1;
-            int hi = N;
-            while(lo<hi){
-                int mid = lo + (hi-lo)/2;
-                if(nums[mid]==target-nums[i]) return {i+1,mid+1};
-                else if(nums[mid]>=target-nums[i]) hi=mid;
-                else lo = mid+1;
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int i=0;
+        int j=numbers.size()-1;
+        vector<int>satwik;
+        
+        while(i<j)
+        {
+            if(numbers[i]+numbers[j]>target) j--;
+            else if(numbers[i]+numbers[j]<target) i++;
+            else
+            {
+                satwik.push_back(i+1);
+                satwik.push_back(j+1);
+                break;
             }
-            if(lo!=nums.size() && nums[lo]==target-nums[i]) return {i+1,lo+1};
         }
-        return {}; // not found
+        
+        return satwik;
     }
 };
