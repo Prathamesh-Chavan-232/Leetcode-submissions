76260c90dfa343100e320dad8c691330634fee2d
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-07-28 03:51:45 +0530

    Time: 14 ms (81.75%), Space: 12.9 MB (5.95%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -1,21 +1,149 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Shortening syntax
+#define ll long long
+#define fo(i, n) for (ll i = 0; i < n; ++i)
+#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
+#define pb push_back
+#define mp make_pair
+#define pqb priority_queue < int>
+#define pqs priority_queue<int, vector < int>, greater < int>>
+#define all(x) x.begin(), x.end()
+#define clr(x) memset(x, 0, sizeof(x))
+#define foreach(it, a) for (auto it = a.begin(); it != a.end(); it++)
+#define formap(m) for (auto[key, value]: m)
+        mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
+
+// Typdefs for containers
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pl;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pii> vpii;
+typedef vector<pl> vpl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+
+// Container input tools
+void inVec(vector<int> &v)
+{
+    string s;
+    getline(cin, s);
+    int num = 0, sign = 1;
+
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] == '[' || s[i] == ']')
+            continue;
+        if (s[i] == '-')
+            sign = -1;
+        else if (s[i] == ',')
+            v.push_back(num), num = 0;
+        else
+        {
+            num = num *10 + (s[i] - '0');
+            num *= sign;
+            sign = 1;
+        }
+    }
+    v.push_back(num);
+}
+
+// Varidiac Variable debugger
+#ifndef ONLINE_JUDGE
+#define debug(...) logger(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...)
+#endif
+template < typename...Args >
+    void logger(string varname, Args && ...values)	// logger for varadiac debugging print statements
+{
+
+    cerr << varname << " =";
+    string delim = " ";
+    (..., (cerr << delim << values, delim = ", "));
+    cerr << "\n";
+}
+
+// STL vector / set (of any type) debugger
+#ifndef ONLINE_JUDGE
+#define debcon(x)\
+cerr << #x << " = ";\
+_print(x);\
+cerr << "\n";
+#else
+#define debcon(x)
+#endif
+template < typename T>
+    void _print(T
+        const &c)
+    {
+        cerr << "{ ";
+        foreach(it, c)
+        {
+            cerr << *it << ", ";
+        }
+        cerr << "}";
+    }
+
+// Function definitions
+void Add_edge(int v1, int v2);
+void dfs(int vertex);
+
+// constants
+const int mod = 1000000007;
+const int N = 1e5 + 10,
+    M = N;
+const double PI = 3.1415926535897932384626;
+
+vpii graph[N];	// For Adjacency List
+bool vis[N];
+
+/*
+    Link -
+    Problem -
+    Difficulty -
+    topic -
+    Status -
+    Date -
+*/
+/* Approach -
+
+*/
+
 class Solution
 {
     public:
-        vector<int> twoSum(vector<int> &nums, int target)
+        vector<int> twoSum(vector<int> &arr, int target)
         {
-            vector<int> res;
-            int n = nums.size();
-            for (int i = 0; i < n; i++)
+            vi res, nums;
+            int n = arr.size();
+            nums = arr;
+            sort(all(nums));
+            int start = 0, end = n - 1, n1, n2;
+
+            while (start < end)
             {
-                for (int j = i + 1; j < n; j++)
+                if (nums[start] + nums[end] == target)
                 {
-                    if (nums[i] + nums[j] == target)
-                    {
-                        res.push_back(i);
-                        res.push_back(j);
-                        break;
-                    }
+                    n1 = nums[start];
+                    n2 = nums[end];
+                    break;
                 }
+                else if (nums[start] + nums[end] < target)
+                {
+                    start++;
+                }
+                else
+                {
+                    end--;
+                }
+            }
+            for (int i = 0; i < n; ++i)
+            {
+                if (n1 == arr[i] || n2 == arr[i])
+                    res.pb(i);
             }
             return res;
         }
