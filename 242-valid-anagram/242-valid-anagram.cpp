91913428e2fccf72bd872ce12c2ebe9c0e9a86dc
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-07-29 15:14:39 +0530

    Time: 30 ms (7.78%), Space: 7.4 MB (45.59%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,9 @@
+#define all(x) x.begin(), x.end()
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        sort(all(s));
+        sort(all(t));
+        return s == t;
+    }
+};
