6c08f9e0d674e305b36d5ee4c8e110383769bea4
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-09-05 23:47:36 +0530

    Time: 23 ms (7.69%), Space: 13.7 MB (63.77%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,167 +1,72 @@
-#include <bits/stdc++.h>
-using namespace std;
 
-// Shortening syntax
-#define ll long long
-#define fo(i, n) for (ll i = 0; i < n; ++i)
-#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
-#define pb push_back
-#define mp make_pair
-#define pqb priority_queue<int>
-#define pqs priority_queue<int, vector<int>, greater<int>>
-#define all(x) x.begin(), x.end()
-#define clr(x) memset(x, 0, sizeof(x))
-#define foreach(it, a) for (auto it = a.begin(); it != a.end(); it++)
-#define formap(m) for (auto [key, value] : m)
-mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-// Typdefs for containers
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pl;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pl> vpl;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-
-// Varidiac Variable debugger
-#ifndef ONLINE_JUDGE
-#define debug(...) logger(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define debug(...)
-#endif
-template <typename... Args>
-void logger(string varname, Args &&...values) // logger for varadiac debugging print statements
-{
-
-    cerr << varname << " =";
-    string delim = " ";
-    (..., (cerr << delim << values, delim = ", "));
-    cerr << "\n";
-}
-
-// STL vector / set (of any type) debugger
-#ifndef ONLINE_JUDGE
-#define debcon(x)        \
-    cerr << #x << " = "; \
-    _print(x);           \
-    cerr << "\n";
-#else
-#define debcon(x)
-#endif
-template <typename T>
-void _print(T const &c)
-{
-    cerr << "{ ";
-    foreach (it, c)
-    {
-        cerr << *it << ", ";
-    }
-    cerr << "}";
-}
-
-// Function definitions
-void Add_edge(int v1, int v2);
-void dfs(int vertex);
-
-// constants
-const int mod = 1'000'000'007;
-const int N = 1e5 + 10, M = N;
-const double PI = 3.1415926535897932384626;
-
-vpii graph[N]; // For Adjacency List
-bool vis[N];
-
-/*
-    Link - https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-    Problem - find the first and last position of repeated element
-    Difficulty - medium
-    topic - Binary search
-    Status - solved
-    Date - 25/5/22
-*/
-/*  Approach -
-
-*/
-
+// classes & functions
 class Solution
 {
 public:
-    int lb(vector<int> &arr, int target)
+    int lb(vector<int> &nums, int target)
     {
-        int mid, res = -1, l = 0, r = arr.size() - 1;
-
-        debug(l, r);
-        if (arr.size() == 0)
-            return res;
-        while (r - l > 1)
+        int res = -1;
+        int lo = 0, hi = nums.size() - 1;
+        while (hi - lo > 1)
         {
-            mid = l + (r - l) / 2;
-            debug(l, mid, r);
-            if (arr[mid] < target)
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] < target)
             {
-                l = mid + 1;
+                lo = mid + 1;
             }
             else
             {
-                r = mid;
+                hi = mid;
             }
         }
-        if (arr[r] == target)
+        if (nums[hi] == target)
         {
-            res = r;
+            res = hi;
         }
-        if (arr[l] == target)
+        if (nums[lo] == target)
         {
-            res = l;
+            res = lo;
         }
-        debug(res);
         return res;
     }
-    int ub(vector<int> &arr, int target)
+    int ub(vector<int> &nums, int target)
     {
-        int mid, res = -1, l = 0, r = arr.size() - 1;
-        while (r - l > 1)
+        int res = -1;
+        int lo = 0, hi = nums.size() - 1;
+        while (hi - lo > 1)
         {
-            mid = l + (r - l) / 2;
-            if (arr[mid] <= target)
+            int mid = lo + (hi - lo) / 2;
+            if (nums[mid] <= target)
             {
-                l = mid + 1;
+                lo = mid;
             }
             else
             {
-                r = mid;
+                hi = mid - 1;
             }
         }
-        debug(arr[l], arr[r]);
-        if (arr[r] > target)
+        if (nums[lo] == target)
         {
-            res = r - 1;
+            res = lo;
         }
-        if (arr[l] > target)
+        if (nums[hi] == target)
         {
-            res = l - 1;
-        }
-        debug(res);
-        if (res == -1)
-        {
-            res = arr.size() - 1;
+            res = hi;
         }
         return res;
     }
-    vector<int> searchRange(vector<int> &arr, int target)
+    vector<int> searchRange(vector<int> &nums, int target)
     {
-
-        vi ans;
-        int temp;
-        temp = lb(arr, target);
-        ans.pb(temp);
-        if (temp != -1)
-        {
-            temp = ub(arr, target);
+        vector<int> ans;
+        if(nums.size() == 0) {
+            ans.push_back(-1);
+            ans.push_back(-1);
+            return ans;
         }
-        ans.pb(temp);
+        int lwb = lb(nums, target);
+        int upb = ub(nums, target);
+        ans.push_back(lwb);
+        ans.push_back(upb);
         return ans;
     }
 };
