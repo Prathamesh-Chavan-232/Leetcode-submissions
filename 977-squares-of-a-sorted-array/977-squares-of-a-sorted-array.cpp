ce87af631a4c1a7e8da451c625562433d56c2616
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-09-16 16:59:45 +0530

    Time: 75 ms (24.20%), Space: 26.9 MB (25.85%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -3,110 +3,55 @@ using namespace std;
 
 // Shortening syntax
 #define ll long long
-#define fo(i, n) for (ll i = 0; i < n; ++i)
-#define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
-#define foreach(it, a) for (auto it = a.begin(); it != a.end(); it++)
-
 // Shortenting stl function calls
 #define pb push_back
-#define mp make_pair
 #define all(x) x.begin(), x.end()
-
 // Typdefs for containers
-typedef pair<int, int> pii;
-typedef pair<ll, ll> pl;
 typedef vector<int> vi;
 typedef vector<ll> vl;
-typedef vector<pii> vpii;
-typedef vector<pl> vpl;
-typedef vector<vi> vvi;
-typedef vector<vl> vvl;
-typedef priority_queue<int> pqb;
-typedef priority_queue<int, vector<int>, greater<int>> pqs;
-
-// Container input tools
-void inVec(vector<int> &v)
-{
-    string s;
-    getline(cin, s);
-    int num = 0, sign = 1;
-
-    for (int i = 0; i < s.size(); ++i)
-    {
-        if (s[i] == '[' || s[i] == ']')
-            continue;
-        if (s[i] == '-')
-            sign = -1;
-        else if (s[i] == ',' || s[i] == ' ')
-            v.push_back(num), num = 0;
-        else
-        {
-            num = num * 10 + (s[i] - '0');
-            num *= sign;
-            sign = 1;
-        }
-    }
-    v.push_back(num);
-}
-
-// Varidiac Variable debugger
-#ifndef ONLINE_JUDGE
-#define debug(...) logger(#__VA_ARGS__, __VA_ARGS__)
-#else
-#define debug(...)
-#endif
-template <typename... Args>
-void logger(string varname, Args &&...values) // logger for varadiac debugging print statements
-{
-
-    cerr << varname << " =";
-    string delim = " ";
-    (..., (cerr << delim << values, delim = ", "));
-    cerr << "\n";
-}
-
-// debugger for STL vector / set (of any type)
-#ifndef ONLINE_JUDGE
-#define debcon(x)        \
-    cerr << #x << " = "; \
-    _print(x);           \
-    cerr << "\n";
-#else
-#define debcon(x)
-#endif
-template <typename T>
-void _print(T const &c)
-{
-    cerr << "{ ";
-    foreach (it, c)
-    {
-        it != --c.end() ? cerr << *it << ", " : cerr << *it;
-    }
-    cerr << "}";
-}
 
 /**
- * @brief
+ * @brief- Given a sorted array with both negative & positive numbers,
+ *         Return a array with a
  *
  */
 // classes & functions
 class Solution
 {
 public:
-    vector<int> sortedSquares(vector<int> &nums)
+    vector<int> sortedSquares(vector<int> &arr)
+    {
+        vi res;
+        int n = arr.size();
+        int l = 0, r = n - 1;
+        for (int i = 0; i < n; ++i)
+        {
+            int left = abs(arr[l]);
+            int right = abs(arr[r]);
+            if (left < right)
+            {
+                res.pb(right * right);
+                r--;
+            }
+            else
+            {
+                res.pb(left * left);
+                l++;
+            }
+        }
+        reverse(all(res));
+        return res;
+    }
+
+    // Sqaure each element & sort the array again.
+    vector<int> sortedSquares2(vector<int> &nums)
     {
         int n = nums.size();
         for (int i = 0; i < n; i++)
         {
             nums[i] *= nums[i];
         }
-        debcon(nums);
         sort(all(nums));
-        debcon(nums);
         return nums;
-    }
-    vector<int> sortedSquares2(vector<int> &nums)
-    {
-        return {};
     }
 };
