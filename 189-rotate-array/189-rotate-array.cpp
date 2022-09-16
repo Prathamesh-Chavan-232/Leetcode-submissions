165344d661855ad64d7db6a3b9f8b3881041084a
falcon_codes <86522183+Prathamesh-Chavan-232@users.noreply.github.com>
2022-09-16 17:42:04 +0530

    Time: 71 ms (14.34%), Space: 25 MB (83.53%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -7,7 +7,7 @@ using namespace std;
 #define Fo(i, k, n) for (ll i = k; k < n ? i < n : i > n; k < n ? ++i : --i)
 #define foreach(it, a) for (auto it = a.begin(); it != a.end(); it++)
 
-// Shortenting stl function calls
+           	// Shortenting stl function calls
 #define pb push_back
 #define mp make_pair
 #define all(x) x.begin(), x.end()
@@ -22,8 +22,7 @@ typedef vector<pl> vpl;
 typedef vector<vi> vvi;
 typedef vector<vl> vvl;
 typedef priority_queue<int> pqb;
-typedef priority_queue<int, vector<int>, greater<int>> pqs;
-
+typedef priority_queue<int, vector < int>, greater < int>> pqs;
 
 // Varidiac Variable debugger
 #ifndef ONLINE_JUDGE
@@ -31,8 +30,8 @@ typedef priority_queue<int, vector<int>, greater<int>> pqs;
 #else
 #define debug(...)
 #endif
-template <typename... Args>
-void logger(string varname, Args &&...values) // logger for varadiac debugging print statements
+template < typename...Args >
+    void logger(string varname, Args && ...values)	// logger for varadiac debugging print statements
 {
 
     cerr << varname << " =";
@@ -43,78 +42,80 @@ void logger(string varname, Args &&...values) // logger for varadiac debugging p
 
 // debugger for STL vector / set (of any type)
 #ifndef ONLINE_JUDGE
-#define debcon(x)        \
-    cerr << #x << " = "; \
-    _print(x);           \
-    cerr << "\n";
+#define debcon(x)\
+cerr << #x << " = ";\
+_print(x);\
+cerr << "\n";
 #else
 #define debcon(x)
 #endif
-template <typename T>
-void _print(T const &c)
-{
-    cerr << "{ ";
-    foreach (it, c)
+template < typename T>
+    void _print(T
+        const &c)
     {
-        it != --c.end() ? cerr << *it << ", " : cerr << *it;
+        cerr << "{ ";
+        foreach(it, c)
+        {
+            it != --c.end() ? cerr << *it << ", " : cerr << *it;
+        }
+        cerr << "}";
     }
-    cerr << "}";
-}
 
 // Data structures
 struct BinTree
 {
     int val;
-    BinTree *left;
-    BinTree *right;
+    BinTree * left;
+    BinTree * right;
     BinTree() {}
-    BinTree(int val) : val(val), left(nullptr), right(nullptr) {}
+    BinTree(int val): val(val), left(nullptr), right(nullptr) {}
 };
 
 // Function declarations
-BinTree *createTree(vector<int> &nodes);
+BinTree* createTree(vector<int> &nodes);
 
 /**
- * @brief
+ *@brief
  **APPROACH 1 :
  If k>=n do k = k%n
 
  **APPROACH 2 : Reversal of array
 
- *   Reverse the whole array.
- *   Then reverse the first k elements.
- *   Finally reverse the remaining elements.
+ *  Reverse the whole array.
+ *  Then reverse the first k elements.
+ *  Finally reverse the remaining elements.
  */
-// classes & functions
+// classes &functions
+class Solution2
+{
+    public:
+        void rotate(vector<int> &nums, int k)
+        {
+            int n = nums.size();
+            if (k > n)
+            {
+                k = k % n;
+            }
+            if (k == 0)
+                return;
+            debug(k, n);
+            int r = n - k;
+            int l = 0;
+            vi res;
+            for (int i = r; i < n; ++i)
+            {
+                res.pb(nums[i]);
+            }
+            for (int i = l; i < r; ++i)
+            {
+                res.pb(nums[i]);
+            }
+            debcon(res);
+            nums = res;
+        }
+};
 class Solution
 {
-public:
-    void rotate(vector<int> &nums, int k)
-    {
-        int n = nums.size();
-        if (k > n)
-        {
-            k = k % n;
-        }
-        if (k == 0)
-            return;
-        debug(k, n);
-        int r = n - k;
-        int l = 0;
-        vi res;
-        for (int i = r; i < n; ++i)
-        {
-            res.pb(nums[i]);
-        }
-        for (int i = l; i < r; ++i)
-        {
-            res.pb(nums[i]);
-        }
-        debcon(res);
-        nums = res;
-    }
-    class Solution2
-    {
     public:
         void rotate(vector<int> &nums, int k)
         {
@@ -123,5 +124,4 @@ public:
             reverse(nums.begin(), nums.begin() + k);
             reverse(nums.begin() + k, nums.end());
         }
-    };
 };
