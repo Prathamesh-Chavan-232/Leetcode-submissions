1a0fb81850484e2b496a0e1cb93abcad70c1b72b
falcon_codes <hulkbusterpowerd@gmail.com>
2024-04-05 17:45:13 +0530

    Time: 4 ms (90.98%) | Memory: 8.6 MB (42.71%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,12 +1,24 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) {
-        if(s.size() != t.size()) {
+    bool isAnagram(string str1, string str2) {
+        if(str1.size() != str2.size()) 
             return false;
-        }
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-        
-        return s == t;    
+
+       vector<int> freq(26);
+       bool res = true;
+       for(int i = 0; i < str1.size(); ++i) {
+        int index = str1[i] - 'a';
+        freq[index]++;
+
+        index = str2[i] - 'a';
+        freq[index]--;
+       }
+       for(int i = 0; i < freq.size(); ++i) {
+            if(freq[i] != 0) {
+                res = false;
+                break;
+            }
+       }
+       return res;
     }
 };
