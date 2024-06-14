032d076d4891b3f2b4947075fb39b6a225a5c7de
falcon_codes <hulkbusterpowerd@gmail.com>
2024-06-14 18:04:36 +0530

    Time: 9 ms (36.89%) | Memory: 21.2 MB (17.84%) - LeetSync

--- 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
+++ 2148-minimum-number-of-moves-to-seat-everyone/minimum-number-of-moves-to-seat-everyone.cpp
@@ -1,13 +1,34 @@
 class Solution {
 public:
     int minMovesToSeat(vector<int>& seats, vector<int>& students) {
-        sort(seats.begin(), seats.end());
-        sort(students.begin(), students.end());
+        int n = seats.size();
+        int maxIndex = max(findMaxInArray(seats), findMaxInArray(students));
+        vector<int> countSeats(maxIndex + 1, 0), countStudents(maxIndex + 1, 0);
 
-        int minMoves = 0;
-        for(int i = 0; i < students.size(); i++) {
-            minMoves += abs(students[i]-seats[i]);
+        for (int i = 0; i < n; i++) {
+            countSeats[seats[i]]++;
+            countStudents[students[i]]++;
+        }
+
+        int i = 0, j = 0, minMoves = 0;
+        while (i <= maxIndex && j <= maxIndex) {
+            if (countSeats[i] != 0 && countStudents[j] != 0) {
+                minMoves += abs(i - j);
+                countSeats[i]--;
+                countStudents[j]--;
+            }
+            if (i <= maxIndex && countSeats[i] == 0)
+                i++;
+            if (j <= maxIndex && countStudents[j] == 0) 
+                j++;
         }
         return minMoves;
+    }
+    int findMaxInArray(vector<int>& arr) {
+        int _max = arr[0];
+        for (int i = 1; i < arr.size(); i++) {
+            _max = max(_max, arr[i]);
+        }
+        return _max;
     }
 };
