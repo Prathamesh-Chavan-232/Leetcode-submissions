d62b008da297e5102b2f8985ece1baa356ea9519
falcon_codes <hulkbusterpowerd@gmail.com>
2024-07-14 21:32:31 +0530

    Time: 98 ms (49.37%) | Memory: 13.1 MB (47.74%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,23 +1,25 @@
+const int N = 2515;
+int dp[N];
+
 class Solution {
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        std::vector<int> tails(nums.size(), 0);
-        int size = 0;
+    int lis(vector<int>& nums, int last) {
+        if (dp[last] != -1)
+            return dp[last];
 
-        for (int x : nums) {
-            int i = 0, j = size;
-            while (i != j) {
-                int m = (i + j) / 2;
-                if (tails[m] < x)
-                    i = m + 1;
-                else
-                    j = m;
-            }
-            tails[i] = x;
-            if (i == size) ++size;
+        int res = 1;
+        for (int i = 0; i < last; i++) {
+            if (nums[last] > nums[i])
+                res = max(res, lis(nums, i) + 1);
         }
-        return size;
+        return dp[last] = res;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        memset(dp, -1, sizeof(dp));
+        int ans = 1;
+        for (int i = 0; i < nums.size(); i++) {
+            ans = max(ans, lis(nums, i));
+        }
+        return ans;
     }
 };
-
-        
